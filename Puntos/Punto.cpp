8a14832ef09f3a137f1979b6031cb32953069727
elenaaalmg <89266114+elenaaalmg@users.noreply.github.com>
2021-10-13 01:26:12 -0500

    Add files via upload

---
+++ Puntos/Punto.cpp
@@ -0,0 +1,44 @@
+//
+// Created by elena on 12/10/2021.
+//
+#include <iostream>
+#include <math.h>
+#define PI 3.14159265
+#include "Punto.h"
+
+using namespace std;
+
+Punto2D::Punto2D() {
+    a = 0;
+    b = 0;
+    c = 0;
+}
+
+void Punto2D::SetPosicion(float x,float y){
+    a = x;
+    b = y;
+}
+
+void Punto2D::Trasladar(float temp1, float temp2) {
+    a = a + temp1;
+    b = b + temp2;
+}
+
+void Punto2D::RotarRespectoAlOrigen(float temp1){
+    c = a;
+    a = c*cos(temp1*PI/180) - b*sin(temp1*PI/180);
+    b = c*sin(temp1*PI/180) + b*cos(temp1*PI/180);
+}
+
+void Punto2D::Escalar(float temp1, float temp2) {
+    a = a*temp1;
+    b = b*temp2;
+}
+
+float Punto2D::GetX() {
+    return a;
+}
+
+float Punto2D::GetY() {
+    return b;
+}
---
+++ Puntos/Punto.h
@@ -0,0 +1,26 @@
+//
+// Created by elena on 12/10/2021.
+//
+
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
+#include <vector>
+
+using namespace std;
+
+class Punto2D {
+public:
+    Punto2D();
+    void SetPosicion(float x, float y);
+    void Trasladar(float temp1, float temp2);
+    void RotarRespectoAlOrigen(float temp1);
+    void Escalar(float temp1, float temp2);
+    float GetX();
+    float GetY();
+
+private:
+    float a, b, c;
+};
+
+
+#endif //PUNTOS_PUNTO_H
